00453ccfd72ae0d9c5fde89db5a6e10ed7540dbd
Jessica Stapp <stapp9@gmail.com>
2019-10-16 15:19:46 -0700

    adjust starte function and add countdown function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,10 @@ window.onload = function() {
   $("#start").on("click", start);
 };
 
+var timeLeft = 30;
+var intervalId;
+var clockRunning = false;
+
 function hideQuestions() {
   $("#question1").hide();
   $("#question2").hide();
@@ -15,6 +19,15 @@ function hideQuestions() {
   $("#question8").hide();
 }
 
+function countdown() {
+  timeLeft--;
+  $("#timer").text("Time left: " + timeLeft);
+}
+
 function start() {
-  $("#timer").show();
+  if (!clockRunning) {
+    clockRunning = true;
+    $("#timer").show();
+    intervalId = setInterval(countdown, 1000);
+  }
 }
